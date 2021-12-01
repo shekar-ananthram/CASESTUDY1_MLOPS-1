07335ef41734d3d6afc564bd32625e1239c97ca7
shekar-ananthram <shekar.ananthram@gmail.com>
2021-12-01 16:44:57 +0530

    Update train_and_evaluate.py

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -50,7 +50,7 @@ def train_and_evaluate(config_path) :
     predicted_qualities = lr.predict(test_x)
 
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
-
+    print("ElasticNet model (alpha=%f, l1_ratio=%f :" %(alpha, l1_ratio))
     print("RMSE : %s" % rmse)
     print("MAE : %s" % mae)
     print("R2 : %s" % r2)
